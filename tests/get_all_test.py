778fb7e2ab1a87913d8624805d3c879624881dc3
Yi Zhang <zy2803414747@gmail.com>
2024-10-31 22:57:44 -0400

    Added more tests for get_all_test

--- tests/get_all_test.py
+++ tests/get_all_test.py
@@ -25,12 +25,30 @@ def test_songs():
     return generate_test_songs_data()
 
 @patch('pandas.read_csv')
-def test_get_songs_by_genre_length(mock_read_csv, test_songs):
+def test_get_songs_by_genre_length_less(mock_read_csv, test_songs):
     mock_read_csv.return_value = test_songs
     
     # Call get_songs_by_genre with n=5
     result = get_songs_by_genre(5)
     assert len(result) == 5, "The function should return exactly 5 songs."
+
+@patch('pandas.read_csv')
+def test_get_songs_by_genre_length_exact_amount(mock_read_csv, test_songs):
+    mock_read_csv.return_value = test_songs
+
+    # Call get_songs_by_genre with exact amount of songs in database
+    result = get_songs_by_genre(10)
+
+    assert len(result) == 10  # Only 10 available
+
+@patch('pandas.read_csv')
+def test_get_songs_by_genre_length_zero_amount(mock_read_csv, test_songs):
+    mock_read_csv.return_value = test_songs
+
+    # Call get_songs_by_genre with zero amount of songs in database
+    result = get_songs_by_genre(0)
+
+    assert len(result) == 0  # should return an empty list
 
 @patch('pandas.read_csv')
 def test_get_songs_variety(mock_read_csv, test_songs):
@@ -42,7 +60,7 @@ def test_get_songs_variety(mock_read_csv, test_songs):
 
     # This test checks if different songs are being fetched on different calls
     assert set(result1['track_name']) != set(result2['track_name']), "The selections should vary."
-
+    
 
 class Tests(unittest.TestCase):
 
