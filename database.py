813db4f1cf981f90ae968ecb794af3fc30ad0243
Kii4ka <Kii4ka@users.noreply.github.com>
2024-11-14 23:13:11 +0000

    Apply autopep8 fixes

--- database.py
+++ database.py
@@ -2,14 +2,14 @@ import sqlite3
 
 
 def create_connection(db_file="songs.db"):
-	conn = sqlite3.connect(db_file)
-	return conn
+    conn = sqlite3.connect(db_file)
+    return conn
 
 
 def create_table():
-	conn = create_connection()
-	cursor = conn.cursor()
-	cursor.execute('''
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute('''
         CREATE TABLE IF NOT EXISTS songs (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             chart_name TEXT,
@@ -19,25 +19,25 @@ def create_table():
             weeks_on_chart INTEGER
         )
     ''')
-	conn.commit()
-	conn.close()
+    conn.commit()
+    conn.close()
 
 
 def insert_song(data):
-	conn = create_connection()
-	cursor = conn.cursor()
-	cursor.execute('''
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute('''
         INSERT INTO songs (chart_name, rank, title, artist, weeks_on_chart)
         VALUES (?, ?, ?, ?, ?)
     ''', (data['chart_name'], data['rank'], data['title'], data['artist'],
-		  data.get('weeks_on_chart', None)))
-	conn.commit()
-	conn.close()
+          data.get('weeks_on_chart', None)))
+    conn.commit()
+    conn.close()
 
 
 def clear_data():
-	conn = create_connection()
-	cursor = conn.cursor()
-	cursor.execute('DELETE FROM songs')
-	conn.commit()
-	conn.close()
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM songs')
+    conn.commit()
+    conn.close()
