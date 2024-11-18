6bae573b24a04782b30e23b951d60ef8437db30d
Kii4ka <evilkomir@gmail.com>
2024-11-17 20:15:29 -0500

    Add EDA functionality for getting top songs (#4)

---
+++ src/eda.py
@@ -0,0 +1,9 @@
+import sqlite3
+import pandas as pd
+
+
+def get_top_songs(conn):
+    query = "SELECT title, artist, chart_name FROM songs WHERE rank = 1 ORDER BY weeks_on_chart DESC LIMIT 10"
+    df = pd.read_sql(query, conn)
+    return df
+
